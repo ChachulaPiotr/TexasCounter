b69c637397b6ff5230dfb52045ddf8ea3277af1a
DudekPL <33094493+DudekPL@users.noreply.github.com>
2018-05-11 17:41:33 +0000

    v0.5
    Not tested

--- main.asm
+++ main.asm
@@ -20,63 +20,90 @@ RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 ;-------------------------------------------------------------------------------
-; Constant
+; Variables
+;-------------------------------------------------------------------------------
+; R4 - counter value
+; R5 - 0s debouncing
+; R6 - 1s debouncing
 ;-------------------------------------------------------------------------------
 
-LOAD_BUT .set 0x01	;load button
-INC_BUT  .set 0x02  ;increment button
-DEB_VAL  .set 0xFFFF  ;debouncing
+;-------------------------------------------------------------------------------
+; Consts
+;-------------------------------------------------------------------------------
+LOAD_BUT .set 0x01
+INC_BUT  .set 0x01  ;buttons
+DEB0_VAL  .set 0xFFFF  ; debouncing 0s(pressed)
+DEB1_VAL  .set 0xFFFF  ; debouncing 1s(released)
+
+;-------------------------------------------------------------------------------
+; Config
+;-------------------------------------------------------------------------------
+;---Setting I/O ports-----------------------------------------------------------
+	mov.b #0x00, P1DIR	;input(load button)
+	mov.b #0x00, P2DIR 	;input(inc button)
+	mov.b #0x00, P3DIR	;input(hex switches)
+	mov.b #0xff, P4DIR	;output(display)
+;---Enabling interrupts---------------------------------------------------------
+	bis.b #0x01, &P2IES
+    bis.b #0x01, &P2IE
+    bis.b #0x01, &P1IES
+    bis.b #0x01, &P1IE
 
 ;-------------------------------------------------------------------------------
 ; Init
 ;-------------------------------------------------------------------------------
-
-;ports direction
-    mov #0x00, P3DIR  ;input
-    mov #0xFF, P2DIR  ;output
-    mov #0xFC, P1DIR  ;buttons
-;buttons interruptions
-    bis #0x03, &P1IES  ;
-    bis #0x03, &P1IE  ;enable interrupt
-	bis.w #0x08, SR
-;inicjalizacja
-    mov #0x00, R4  ;counter init
-    mov R4, P2OUT  ;output "init"
+    mov.b #0x00, R4  ;setting counter
+    mov.b R4, P4OUT  ;displaying
     jmp SLEEP
 
 ;-------------------------------------------------------------------------------
-; Main loop here
+; Interrupts
 ;-------------------------------------------------------------------------------
+LOAD_INT:
+	bic #CPUOFF|SCG1|SCG0|OSCOFF, 0(SP) ; wake up
+	mov.b P3IN, R4
+	mov.b R4, P4OUT
+	bit.b #LOAD_BUT, P1IN
+	jz LOAD_INT
+;---Simulating RETI-------------------------------------------------------------
+	pop SR
+    inc SP
+;-------------------------------------------------------------------------------
+	bic #LOAD_BUT, P1IFG  ; reset of interrupt flag
+	jmp SLEEP
 
+INC_INT:
+	bic #CPUOFF|SCG1|SCG0|OSCOFF, 0(SP) ; wake up
+	mov.b #DEB0_VAL, R5
+DEBOUNCING0:
+	mov.b #DEB1_VAL, R6
+	bit.b #INC_BUT,P2IN
+	jnz DEBOUNCING1
+	dec.b R5
+	jnz DEBOUNCING0
+INCREMENTATION:
+	DINT
+	dadc.b R4
+	mov.b R4, P4OUT
+EXIT_INC:
+;---Simulating RETI-------------------------------------------------------------
+	pop SR
+    inc SP
+;-------------------------------------------------------------------------------
+	bic #INC_BUT, P2IFG  ; reset of interrupt flag
+	jmp SLEEP
+DEBOUNCING1:
+	bit.b #INC_BUT,P2IN
+	jz DEBOUNCING0
+	dec.b R6
+	jnz DEBOUNCING1
+	jmp EXIT_INC
+;-------------------------------------------------------------------------------
+; Main loop
+;-------------------------------------------------------------------------------
 SLEEP:
-	bis #CPUOFF|SCG1|SCG0|OSCOFF|GIE, SR ; LMP 4, EI  ;
+	bis #CPUOFF|SCG1|SCG0|OSCOFF|GIE, SR ;EI, LMP4
 	NOP
-	bit #LOAD_BUT, R6	;interrupt report
-	jz LOAD
-	jmp MAIN
-
-MAIN:
-	mov #DEB_VAL, R5
-	bit #LOAD_BUT, P1IN
-	jz COUNT
-
-COUNT:
-	SETC
-	DADC R4	;inc decimally
-	mov R4, P2OUT  ;output
-
-INTERRUPT:
-    bic #CPUOFF|SCG1|SCG0|OSCOFF, 0(SP) ; wake up
-    bic #LOAD_BUT|INC_BUT, P1IFG  ; allow interrupt
-    mov P1IN, R6	;what happened
-    RETI
-
-LOAD:
-	mov P3IN, R4
-    mov R4, P2OUT
-    bit #LOAD_BUT, P1IN	;infinite load (as long as you keep button pressed)
-    jz LOAD
-    jmp SLEEP
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
@@ -89,4 +116,8 @@ LOAD:
 ;-------------------------------------------------------------------------------
             .sect   ".reset"                ; MSP430 RESET Vector
             .short  RESET
+            .sect   ".int04"				; high prority interruption(loading)
+            .short  LOAD_INT
+            .sect   ".int01"				; low priority interruption(incrementation)
+            .short  INC_INT
             
